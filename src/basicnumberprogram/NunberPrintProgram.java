9a600b7166f271d0fa2508f17f888717edd18f39
Jitu <Jitu@DESKTOP-RG4B0S8>
2022-06-01 08:41:27 +0530

    numeric

---
+++ src/basicnumberprogram/NunberPrintProgram.java
@@ -0,0 +1,498 @@
+//   package basicnumberprogram;
+//
+//    public class NunberPrintProgram 
+//     {
+//	     Java Program to Check if a Number is Positive or Negative or Zero
+//	
+//		public static void main(String[] args) 
+//		{
+//			int i=10;
+//			
+//			if (i<0)
+//			{
+//			 System.out.println(i+ "Number is Negative");
+//			}
+//			
+//			else if(i>0)
+//			{
+//			 System.out.println(i+ "Number is Positive");
+//			
+//			}
+//			
+//			else
+//			{
+//				System.out.println(i+ "Number is Zero");
+//			}
+//		}
+//	}
+//
+//
+//    	 
+//         multiplication of of value without using "multiply sign"...        
+//
+//		int x=5;
+//		int y=4;
+//		int sum=0; 
+		  
+//		for(int i=1;i<=x;i++)  
+//		{   
+//			sum=sum+y;
+//		}  
+//		System.out.println(sum);  
+//		}
+//
+//
+//
+//
+//
+//
+//	Java Program to find whether number is odd or even
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			int i=5;
+//			
+//			if (i%2==0)
+//			{
+//				System.out.println(i+ "Number is Even");
+//			}
+//			
+//			else
+//			{
+//				System.out.println(i+ "Number is Odd");
+//			}
+//		}
+//	}
+//
+//
+//
+//
+//	Java Program to Display Even Numbers From 1 to 100
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			
+//			for(int i=2;i<=100;i=i+2)
+//			{
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//
+//	Java Program to Display Odd Numbers From 1 to 100
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			
+//			for(int i=1;i<=100;i=i+2)
+//			{
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//
+//	Java Program to Find Largest of Two Numbers
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			
+//			
+//			// Find Largest in two numbers
+//			
+//			int i=25;
+//			int j=85;
+//			
+//			if(i>j)
+//			{
+//				System.out.println(i+ "is Greater");
+//			}
+//			else
+//			{
+//				System.out.println(j+ "is grater");
+//			}
+//		}
+//	}
+//
+//	Java Program to Find Sum of first 10 Natural Numbers
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			
+//			int sum=0;
+//			for(int i=1;i<=10;i++)
+//			{
+//				sum= sum+i;
+//			}
+//			System.out.println("Sum of first 10 Natural Numbers" +sum);
+//		}
+//	}
+//
+
+//
+//	Java Program to Find Largest of Three Numbers
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			
+//			int i=105;
+//			int j= 185;
+//			int k= 195;
+//			
+//			if(i>j)
+//			{
+//				if(i>k)
+//				{
+//				System.out.println("Largest No is:"+i);
+//				}
+//			}
+//			if(j>i)
+//			{
+//				if(j>k)
+//				{
+//				System.out.println("Largest No is:"+j);
+//				}
+//			}
+//			if(k>i)
+//			{
+//				if(k>j)
+//				{
+//				System.out.println("Largest No is:"+k);
+//				}
+//			}		}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	Java Program to Find Smallest of Three Numbers 
+//
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			
+//			int i=205;
+//			int j=85;
+//			int k=95;
+//			
+//			if(i<j && i<k)
+//			{
+//				System.out.println(i+ "is Smaller");
+//			}
+//			else if(j<i && j<k)
+//			{
+//				System.out.println(j+ "is Smaller ");
+//			}
+//			else
+//			{
+//				System.out.println(k+ "is Smaller ");
+//			}
+//		}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	Java Program to Find Square and Sqaure Root of a Number
+//	package basicAssign;
+//
+//	public class PosNig 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			
+//			int i=3;
+//			int j=i*i;
+//			int k= 100;
+//			double sqrt= Math.sqrt(k);
+//			System.out.println(i+ " Square is " +j);
+//		System.out.println(k+ " Square root is " +sqrt);
+//		}
+//	}
+//
+//
+//	How to Print ASCII Value in Java
+//	package basicAssign;
+//
+//	public class AscII 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			int i='A';
+//			int j='K';
+//			
+//			System.out.println("Ascii Value Of A is:"+i);
+//			System.out.println("Ascii Value of B is:"+j);
+//					
+//		}
+//	}
+//
+//
+//
+//
+//	Java Program to Find Largest of Four Numbers(use AND/OR Operator)
+//	package basicAssign;
+//
+//	public class AscII 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			int i= 10;
+//			int j=100;
+//			int k= 205;
+//			int l=95;
+//			
+//			if(i>j && i>k && i>l)
+//			{
+//				System.out.println("Largest No is:" +i);
+//			}
+//			if(j>i && j>k && j>l)
+//			{
+//				System.out.println("Largest No is:" +j);
+//			}
+//			if(k>i && k>j && k>l)
+//			{
+//				System.out.println("Largest No is:" +k);
+//			}
+//			if(l>i && l>j && l>k)
+//			{
+//				System.out.println("Largest No is:" +l);
+//			}
+//		}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//	Java Program to Swap Two Numbers without using third variable
+//
+//	package basicAssign;
+//
+//	public class AscII 
+//	{
+//		public static void main(String[] args) 
+//		{
+//	int i=10;
+//			int j=25;
+//			
+//			i=i+j;
+//			j=i-j;
+//			i=i-j;
+//			
+//			System.out.println("Value of i is: " +i);
+//			System.out.println("Value of j is: " +j);
+//					
+//		}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	Java Program for Fibonacci Series 
+//	package basicAssign;
+//
+//	public class AscII 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			int num1=0;
+//			int num2=1;
+//			int num3;
+//			
+//			for(int i=1;i<=10;i++)
+//			{
+//				System.out.println(num1);
+//				num3=num1+num2;
+//				num1=num2;
+//				num2=num3;
+//			}				
+//		}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	Java Program for Palindrome Number
+//	package basicAssign;
+//
+//	public class AscII 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			int num=8339;
+//			
+//				int i;
+//				int j=0;
+//				
+//		       int k=num;    
+//		       while(num>0)
+//		       {    
+//		    	   i=num%10;    
+//		    	   j=(j*10)+i;    
+//		    	   num=num/10;    
+//		       }    
+//		        if(k==j)
+//		        {
+//		System.out.println(k+" is a palindrome number ");    
+//		        }
+//		        else
+//		        {
+//		  System.out.println(k+" is not a palindrome"); 
+//		        }	
+//	}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	Java Program for Factorial of any number
+//	package basicAssign;
+//
+//	public class AscII 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			int fact=1;  
+//			  for(int i=1;i<=5;i++)
+//			  {    
+//			      fact=fact*i;    
+//			  }    
+//		System.out.println("Factorial of 5 is: "+fact);
+//	}
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	Java Program to Display Prime Numbers between 1 to 100
+//	package basicAssign;
+//
+//	public class AscII 
+//	{
+//		public static void main(String[] args) 
+//		{
+//			for (int i = 1; i <= 100; i++) 
+//			  {
+//			   int count = 0;
+//			   
+//			   for (int j = 2; j <= i / 2; j++) 
+//			   {
+//			    if (i % j == 0) 
+//			    {
+//			     count++;
+//			     break;
+//			    }
+//			  }
+//			 
+//			   if (count == 0)
+//			   {
+//			    System.out.println(i);
+//			   }
+//			  }
+//	}
+//	}
+//
+//		    int n=6;     //it is the number to be checked    
+//			
+//			int m=n/2; 
+//			int temp=0;
+//			
+//		  if(n==0||n==1)
+//		  {  
+//			  System.out.println(n+" is not prime number");      
+//		  }
+//		  
+//		  else
+//		  {  
+//		   for(int i=2;i<=m;i++)
+//		   {      
+//		    if(n%i==0)
+//		    {      
+//		     System.out.println(n+" is not prime number");      
+//		    temp=1;      
+//		     break;     
+//		    }      
+//		   }   
+//		   
+//		   if(temp==0)  
+//		   { 
+//			   System.out.println(n+" is prime number"); 
+//			}  
+//		  }
+// }
